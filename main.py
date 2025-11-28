960196c2c0bd3d76311a278906b7871df2fe269c
Valentino Cervantes Erequita <104030868+KiwiAmenazante@users.noreply.github.com>
2025-11-28 00:07:30 -0500

    chore: integrate eventlet for improved concurrency in socketio

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from app import create_app
 from database.db import ensure_sensors_table
 from mqtt.client import create_mqtt_client
@@ -5,9 +8,11 @@ from config.settings import settings
 
 if __name__ == "__main__":
     ensure_sensors_table()
+
     app, socketio = create_app()
 
     mqtt_client = create_mqtt_client()
     mqtt_client.loop_start()
 
-    socketio.run(app, host="0.0.0.0", port=settings.FLASK_PORT, allow_unsafe_werkzeug=True)
+    # BOOT CON EVENTLET – PERFECTO PARA COOLIFY
+    socketio.run(app, host="0.0.0.0", port=settings.FLASK_PORT)
