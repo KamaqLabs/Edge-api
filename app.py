633535cec117a1ccfad35bd66dc03d0525e2dac4
Valentino Cervantes Erequita <104030868+KiwiAmenazante@users.noreply.github.com>
2025-11-28 00:41:42 -0500

    chore: refactor app to use eventlet for socketio and add Procfile for deployment

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 from flask import Flask
 from flask_socketio import SocketIO
+
+from database.db import ensure_sensors_table
+from mqtt.client import create_mqtt_client
 from routes.sensor_routes import sensor_bp
 from services.websocket_service import set_socketio_instance
 
+ensure_sensors_table()
+mqtt_client = create_mqtt_client()
+mqtt_client.loop_start()
+
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
 def create_app():
-    app = Flask(__name__)
-    socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
-
     app.register_blueprint(sensor_bp)
-
-    # ✔ Después de registrar routes
     set_socketio_instance(socketio)
-
     return app, socketio
+
+
+
+
+app, socketio = create_app()
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ eventlet.monkey_patch()
 from app import create_app
 from database.db import ensure_sensors_table
 from mqtt.client import create_mqtt_client
-from config.settings import settings
 
 if __name__ == "__main__":
     ensure_sensors_table()
@@ -14,5 +13,4 @@ if __name__ == "__main__":
     mqtt_client = create_mqtt_client()
     mqtt_client.loop_start()
 
-    # BOOT CON EVENTLET – PERFECTO PARA COOLIFY
-    socketio.run(app, host="0.0.0.0", port=settings.FLASK_PORT)
+    socketio.run(app, host="0.0.0.0", port=5000)
